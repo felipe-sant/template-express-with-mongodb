f12949cd5e469aa0176e7cb3be0df788c2320894
Luiz Felipe dos Santos <felipe.pinghang2@gmail.com>
2025-12-05 13:37:00 +0000

    #1 feat: adicionando suporte ao env

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import app from "./app"
+import dotenv from "dotenv"
 
+dotenv.config()
+
+const mongouri = process.env.MONGOURI
 const port = 3001
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`✅ | Servidor rodando na porta http://0.0.0.0:${port}`)
+    console.log(`✅ | Conectando ao banco de dados em ${mongouri}`)
 })
