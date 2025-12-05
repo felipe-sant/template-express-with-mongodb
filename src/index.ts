11c6c164738e7f3547f9f8814360cfa48213d220
Luiz Felipe dos Santos <felipe.pinghang2@gmail.com>
2025-12-05 13:43:21 +0000

    #1 feat: adicionando conexao com o mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import mongoose from "mongoose"
 import app from "./app"
 import dotenv from "dotenv"
 
@@ -6,7 +7,12 @@ dotenv.config()
 const mongouri = process.env.MONGOURI
 const port = 3001
 
+mongoose.connect(mongouri as string)
+    .then(() => console.log("✅ | Conectado ao banco de dados"))
+    .catch((err) => console.log("❌ | Erro ao conectar ao banco de dados: ", err))
+
 app.listen(port, '0.0.0.0', () => {
     console.log(`✅ | Servidor rodando na porta http://0.0.0.0:${port}`)
-    console.log(`✅ | Conectando ao banco de dados em ${mongouri}`)
+}).on('error', (err) => {
+    console.log("❌ | Erro ao iniciar o servidor: ", err)
 })
